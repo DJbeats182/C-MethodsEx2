825da9effe383f0daad578e1814ee6f887d3640c
DJ Bates <blooviper@gmail.com>
2022-08-08 14:43:26 -0600

    made a few changes

---
+++ CSharpMethodsEX2Console/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMethodsEX2Console
+{
+    internal class Methods
+    {
+        public static int Sum(int num1, int num2) // Adds two numbers together
+        {
+            int answer = num1 + num2;
+            return answer;
+        }
+
+        public static void Add()
+        {
+            Console.WriteLine("Enter in a number:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter in a second number:");
+            int num2 = int.Parse(Console.ReadLine());
+            var answer = num1 + num2;   
+            Console.WriteLine($"{num1} + {num2} = {answer}");
+        }
+
+        public static int Multiply(int num1, int num2) // Multiplies two numbers together
+        {
+            return num1 * num2;
+        }
+
+        public static int Subtract(int num1, int num2) // Subtracts two numbers 
+        {
+            return num1 - num2;
+        }
+
+        public static int Divide(int num1, int num2) // Divides two numbers 
+        {
+            return num1/ num2;
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2; 
+        }
+
+        public static int SumVal(params int[] values)
+        {
+            int sum = 0;
+            foreach (int val in values)
+            {
+                sum += val;
+            }
+            return sum;
+        }
+
+        public static int SubVals(params int[] list)
+        {
+            var startingPoint = list[0];
+            for (int i = 1; i < list.Length; i++)
+            {
+                startingPoint -= list[i];
+            }
+            return startingPoint;
+        }
+    }
+}
--- CSharpMethodsEX2Console/Program.cs
+++ CSharpMethodsEX2Console/Program.cs
@@ -2,23 +2,28 @@
 {
     internal class Program
     {
-        public static int Sum(int num1, int num2)
+        
+        public static void Main(string[] args)
         {
-            int answer = num1 + num2; 
-            return answer;
-        }
-        public static int Multiply(int num1, int num2)
-        {
-            return num1 * num2;
-        }
-        static void Main(string[] args)
-        {
-            var pizzaPrices = Sum(2, 4);
-          
-            var pizzaToppings = Multiply(2, 10);
+            //--------------------------Methods-Exercise-2---------------------------
 
-            Console.WriteLine(pizzaPrices);
-            Console.WriteLine(pizzaToppings);
+            int sum = Methods.Sum(1, 1);
+            Console.WriteLine($"The sum of num1 and num2 is {sum}");
+            int difference = Methods.Subtract(2,2);
+            Console.WriteLine($"The difference of num1 and num2 is {difference}");
+            int product = Methods.Multiply(2,2);
+            Console.WriteLine($"The product of num1 and num2 is {product}");
+            int quotient = Methods.Divide(10,2);
+            Console.WriteLine($"The quotient of num1 and num2 is {quotient}");
+            Methods.Add();
+            
+
+            //--------------------------Challenge-Mode-------------------------------
+                     
+            
+
+            Console.WriteLine(Methods.SumVal(1, 1, 1, 3)); // takes each number in the variabled list and adds them together
+            Console.WriteLine(Methods.SubVals(10,2,1,1));  // takes each number in the variabled list and subtracts them together
         }
     }
 }
